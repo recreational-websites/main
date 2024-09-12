30494f6c05166379ee802712260d1b8a3847f68e
Juyeong Maing <mjy9088@naver.com>
2024-09-12 14:33:15 +0900

    remove rewrite rules

--- main/next.config.js
+++ main/next.config.js
@@ -1,33 +1,5 @@
-function env(...names) {
-  for (const name of names) {
-    const result = process.env[name];
-    if (typeof result === "string") return result;
-  }
-  throw new Error(`Error: missing environment variable: ${names}`);
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/polyomino",
-        destination: `${env("POLYOMINO_URL")}/polyomino`,
-      },
-      {
-        source: "/polyomino/:path+",
-        destination: `${env("POLYOMINO_URL")}/polyomino/:path+`,
-      },
-      {
-        source: "/color",
-        destination: `${env("COLOR_URL")}/color`,
-      },
-      {
-        source: "/color/:path+",
-        destination: `${env("COLOR_URL")}/color/:path+`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
