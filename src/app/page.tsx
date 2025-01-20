79a0d7922df06b0757f3f5596d61a86a81e7f750
Juyeong Maing <mjy9088@naver.com>
2025-01-20 20:33:38 +0900

    apply changed link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Page() {
   return (
     <ul>
       <li>
-        <a href="/polyomino">Polyomino</a>
+        <a href="/polyform/p4m-s-e">Polyomino</a>
+        <a href="/polyform/p4m-s-v">Polyplet</a>
         <a href="/color">Color</a>
       </li>
     </ul>
