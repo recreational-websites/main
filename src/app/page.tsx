a53f39f6333a8a6fc9cdf01dfd487443253df8c0
Juyeong Maing <mjy9088@naver.com>
2025-02-02 22:17:56 +0900

    fix outdated links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ export default function Page() {
   return (
     <ul>
       <li>
-        <a href="/polyform/p4m-s-e">Polyomino</a>
-        <a href="/polyform/p4m-s-v">Polyplet</a>
+        <a href="/polyform">Polyform</a>
+      </li>
+      <li>
         <a href="/color">Color</a>
+      </li>
+      <li>
+        <a href="/chess">Chess</a>
       </li>
     </ul>
   );
